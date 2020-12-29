a632946988b8605b95e1ec186e306dd5e5e6760f
ps37 <prudhvi.af121@gmail.com>
2020-12-29 14:56:19 -0500

    Import ng live docs module with documentation jsons
    Signed-off-by: ps37 <prudhvi.af121@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExamplesModule } from './examples.module';
 
+import {
+  CompodocSchema,
+  NgLiveDocsModule,
+  StackBlitzInfo,
+} from '@vmw/ng-live-docs';
+import componentsDocumentationJson from '../gen/components-doc.json';
+import examplesDocumentationJson from '../gen/examples-doc.json';
+
+export function moduleFinder(componentName: string): string {
+  return componentName.replace('ExampleComponent', 'ExampleModule');
+}
+
+/**
+ * The following 2 constants are declared for AOT compilation purpose. Otherwise, the compilation would silently fail and
+ * the doc jsons are given as null to the NgLiveDocsModule.
+ * NOTE: The following two has to be exported otherwise the AoT compiler won't see it.
+ */
+export const docJson1: CompodocSchema = (componentsDocumentationJson as unknown) as CompodocSchema;
+export const docJson2: CompodocSchema = (examplesDocumentationJson as unknown) as CompodocSchema;
+export const sbInfo: StackBlitzInfo = {
+  templateId: 'clarity-v4-light-theme',
+  projectName: 'Clarity Angular Examples',
+  moduleFinder,
+};
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -19,6 +44,7 @@ import { ExamplesModule } from './examples.module';
     AppRoutingModule,
     ClarityModule,
     BrowserAnimationsModule,
+    NgLiveDocsModule.forRoot([docJson1, docJson2], sbInfo),
     ExamplesModule,
   ],
   providers: [],
