1a72a380dc44913448c2ccb0375fde4c3091ef4a
ps37 <prudhvi.af121@gmail.com>
2020-12-31 11:34:53 -0500

    Remove clr modal backdrop and basic examples
    Signed-off-by: ps37 <prudhvi.af121@gmail.com>

--- src/app/components/clr-modal/clr-modal.examples.module.ts
+++ src/app/components/clr-modal/clr-modal.examples.module.ts
@@ -7,41 +7,22 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ClrModal } from '@clr/angular';
 import { Documentation } from '@vmw/ng-live-docs';
-import { ClrModalBackdropExampleComponent } from './backdrop/clr-modal-backdrop.example.component';
-import { ClrModalBackdropExampleModule } from './backdrop/clr-modal-backdrop.example.module';
-import { ClrModalBasicExampleComponent } from './basic/clr-modal-basic.example.component';
-import { ClrModalBasicExampleModule } from './basic/clr-modal-basic.example.module';
 import { ClrModalSizesExampleComponent } from './sizes/clr-modal-sizes.example.component';
 import { ClrModalSizesExampleModule } from './sizes/clr-modal-sizes.example.module';
 
 Documentation.registerDocumentationEntry({
-    component: ClrModal,
-    displayName: 'ClrModal',
-    urlSegment: 'clrModal',
-    examples: [
-        {
-            component: ClrModalBasicExampleComponent,
-            title: 'Basic Clarity Modal',
-            urlSegment: 'clr-modal-basic',
-        },
-        {
-            component: ClrModalSizesExampleComponent,
-            title: 'Different sizes of Clarity Modal',
-            urlSegment: 'clr-modal-sizes',
-        },
-        {
-            component: ClrModalBackdropExampleComponent,
-            title: 'Backdrop of Clarity Modal',
-            urlSegment: 'clr-modal-backdrop',
-        },
-    ],
+  component: ClrModal,
+  displayName: 'ClrModal',
+  urlSegment: 'clrModal',
+  examples: [
+    {
+      component: ClrModalSizesExampleComponent,
+      title: 'Different sizes of Clarity Modal',
+      urlSegment: 'clr-modal-sizes',
+    },
+  ],
 });
 @NgModule({
-    imports: [
-        CommonModule,
-        ClrModalBasicExampleModule,
-        ClrModalSizesExampleModule,
-        ClrModalBackdropExampleModule
-    ],
+  imports: [CommonModule, ClrModalSizesExampleModule],
 })
 export class ClrModalExamplesModule {}
